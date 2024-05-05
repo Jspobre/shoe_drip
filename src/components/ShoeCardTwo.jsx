eff92a5d47f5847d235eaed4dbb9b10214a61ba5
Jack Vincent Pobre <jackvincentsanchez.pobre@bicol-u.edu.ph>
2024-05-05 22:22:39 +0800

    recreated hero section

---
+++ src/components/ShoeCardTwo.jsx
@@ -0,0 +1,27 @@
+
+
+const ShoeCardTwo = ({imgUrl, bigShoeImg, changeBigShoeImg}) => {
+
+    const handleClick = () => {
+           if(bigShoeImg !== imgUrl.bigShoe){
+                    changeBigShoeImg(imgUrl.bigShoe)
+           }
+    }
+  return (
+    <div className={
+    `border-2 rounded-xl 
+    ${bigShoeImg === imgUrl.bigShoe ? 'border-coral-red': 'border-transparent'}
+    cursor-pointer max-sm:flex-1`
+    }
+     onClick={handleClick}
+     >
+        <div className="flex justify-center items-center bg-center bg-cover bg-card object-contain
+     rounded-xl flex-1 p-4 sm:w-40 sm:h-40 max-sm:p-4">
+        <img src={imgUrl.bigShoe} alt="" width={127} height={103} className="object-contain" />
+        </div>
+ 
+    </div>
+  )
+}
+
+export default ShoeCardTwo
--- src/sections/MyHero.jsx
+++ src/sections/MyHero.jsx
@@ -1,14 +1,23 @@
+import { bigShoe1, bigShoe2 } from "../assets/images"
+
+import { shoes, statistics } from "../constants"
+import ShoeCardTwo from "../components/ShoeCardTwo"
+import { useState } from "react"
 
 
 const MyHero = () => {
+
+  const [bigShoeImg, setBigShoeImg ] = useState(bigShoe1);
+
+
   return (
     <section id="home" className="max-container w-full flex xl:flex-row
-    gap-10 min-h-screen items-start">
+    gap-10 min-h-screen flex-col">
         <div className="relative w-full flex flex-col justify-center items-start max-xl:padding-x 
-        pt-28 xl:w-2/5">
-            <p className="font-montserrat text-coral-red text-xl leading-normal">Our Summer Collections</p>
-            <h1 className="mt-14 font-palanguin text-8xl font-bold relative "> 
-            <span className="xl:bg-white xl:whitespace-nowrap pr-10">The New Arrival</span> <br/>
+        pt-40 xl:w-2/5 z-10">
+            <p className="font-montserrat text-coral-red text-xl leading-8">Our Summer Collections</p>
+            <h1 className="mt-14 font-palanguin text-8xl font-bold relative"> 
+            <span className="xl:bg-white xl:whitespace-nowrap pr-8 py-4">The New Arrival</span> <br/>
             <span className="text-coral-red">Nike</span> Shoes
             <p className="text-lg font-montserrat font-normal leading-8 text-slate-gray py-5
             sm:max-w-sm">
@@ -18,12 +27,38 @@ const MyHero = () => {
             px-16 py-5 rounded-full">
                 Submit
             </button>
+            <div className="flex mt-10 gap-10 leading-normal flex-wrap w-full">
+              {statistics.map((stat) => (
+                      <div key={stat.label}>
+                        <p className="font-palanquin text-[48px]">{stat.value}</p>
+                        <p className="font-montserrat text-[18px] text-slate-gray font-normal">{stat.label}</p>
+                      </div>
+              ))}
+            </div>
             </h1>
         </div>
-        <div className="max-lg:flex-col relative max-xl:padding pt-28 bg-hero">
-            hello
+        <div className="flex flex-1 relative max-xl:padding justify-center items-center pt-14 bg-hero bg-cover
+        xl:min-h-screen max-xl:py-40 w-full ">
+            <img src={bigShoeImg} 
+            alt=""
+             width={610}
+              height={512} 
+              className="object-contain relative z-10"
+              />
+            <div className="flex justify-center gap-10 items-center absolute bottom-[-5%] max-container
+            max-sm:px-6">
+            {shoes.map((shoe, index) => (
+                   <div key={index}>
+                      <ShoeCardTwo imgUrl={shoe} bigShoeImg={bigShoeImg} 
+                           changeBigShoeImg={(shoe) => 
+                            setBigShoeImg(shoe)}/> 
+                   </div>
+
+               ))}
+            </div>
         </div>
    
+     
     </section>
   )
 }
